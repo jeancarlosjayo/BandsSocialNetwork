72d6737ccb33c84893f8650ab76e1e7c5e5db87a
Jean Carlos Ramirez Jayo <49418665+jeancarlosjayo@users.noreply.github.com>
2022-05-18 04:07:02 -0500

    owo castillo

--- app/src/main/java/com/jramdev/bandssocialnetwork/LoginActivity.kt
+++ app/src/main/java/com/jramdev/bandssocialnetwork/LoginActivity.kt
@@ -1,11 +1,66 @@
 package com.jramdev.bandssocialnetwork
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+    private lateinit var btnLogin: ImageButton;
+    private lateinit var emailEdt: EditText;
+    private lateinit var passwordEdt: EditText;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = Firebase.auth
+        btnLogin = findViewById(R.id.btnLogin)
+        emailEdt = findViewById(R.id.userEditText)
+        passwordEdt = findViewById(R.id.passwordEditText)
+        btnLogin.setOnClickListener {
+            val password: String = passwordEdt.text.toString()
+            val email: String = emailEdt.text.toString()
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("", "signInWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("", "signInWithEmail:failure", task.exception)
+                        updateUI(null)
+                    }
+                }
+        }
     }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+
+    }
+
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            Toast.makeText(this, "Autenticacion exitosa", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Autenticacion fallida", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
